03385144c67f9344f447ca1a536a22e3d234a016
Arder Blackard <arder.blackard@gmail.com>
2019-04-23 04:40:35 +0300

    Added static files copying

--- vue-webpack-4/Client/build/webpack.prod.conf.js
+++ vue-webpack-4/Client/build/webpack.prod.conf.js
@@ -1,8 +1,10 @@
 ﻿const baseConfig = require("./webpack.base.conf");
 const merge = require("webpack-merge");
+const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(baseConfig, {
     mode: "production",
@@ -55,6 +57,13 @@ module.exports = merge(baseConfig, {
         new MiniCssExtractPlugin({
             filename: "css/[name].[chunkhash].css",
             chunkFilename: "css/[id].[chunkhash].css"
-        })
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, "../static"),
+                to: "static",
+                ignore: [".*"]
+            }
+        ])
     ]
 });
